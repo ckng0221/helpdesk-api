0a31ff34860f7a0bafc5735fa0cc1b5cfe659990
CK Ng <ngchoonkhon@gmail.com>
2023-09-17 14:11:57 +0800

    update pipeline

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { TicketModule } from './ticket/ticket.module';
 import { AuthModule } from './auth/auth.module';
 
-const mongoConnection = 'mongodb://localhost/helpdesk';
+const mongoConnection = 'mongodb://localhost:27017/helpdesk';
 
 @Module({
   imports: [
