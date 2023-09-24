8c22cb62ea4e501b4d3aa3b73765d17d11273efc
CK Ng <ngchoonkhon@gmail.com>
2023-09-24 20:18:04 +0800

    Enable swagger plugins, remove unnecessary apiproperty

--- src/main/dto/user.dto.ts
+++ src/main/dto/user.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty, PartialType } from '@nestjs/swagger';
 
 class UserDto {
-  // @ApiProperty()
+  //
   // id?: number;
 
   @ApiProperty({ example: 'alice' })
@@ -19,62 +19,46 @@ class UserDto {
   @ApiProperty({ example: '+6011-1234567' })
   mobilephone?: string;
 
-  @ApiProperty()
   workstation?: string;
 
-  @ApiProperty()
   country?: string;
 
-  @ApiProperty()
   extension?: string;
 
-  @ApiProperty()
   lastlogin_date?: Date;
 
-  @ApiProperty()
   lastpasswordchange_date?: Date;
 
-  @ApiProperty()
   title?: string;
 
-  @ApiProperty()
   outofoffice_message?: string;
 
-  @ApiProperty()
   is_admin?: boolean;
 
-  @ApiProperty()
   is_active?: boolean;
 
   @ApiProperty({ default: false })
   deletion_flag?: boolean;
 
-  @ApiProperty()
   is_out_of_office?: boolean;
 
-  @ApiProperty()
   id_old?: string;
 
   // CMDB related
-  @ApiProperty()
+
   ad_status?: 'ENABLED' | 'DISABLED' | 'OFFBOARD';
 
-  @ApiProperty()
   staff_status?: 'ACTIVE' | 'RESIGNED';
 
-  @ApiProperty()
   entry_date?: Date;
 
-  @ApiProperty()
   leaving_date?: Date;
 
-  @ApiProperty()
   engineer_teams?: string[];
 
-  // @ApiProperty()
+  //
   // manager_id?: number;
 
-  @ApiProperty()
   department?: string[];
 }
 
